a94bc4f2e971e809e7014a15c9fda693368deff4
nihalshahria <53012643+nihalshahria@users.noreply.github.com>
2019-12-25 22:39:28 +0600

    sdsd

---
+++ Personal Practice/Is it a tree.cpp
@@ -0,0 +1,48 @@
+/*
+          _____       __   __   __      __                  __
+         |     \     |  | |  | |  |    |  |       /\       |  |
+         |  |\  \    |  | |  | |  |    |  |      /  \      |  |
+         |  | \  \   |  | |  | |  |____|  |     / /\ \     |  |
+         |  |  \  \  |  | |  | |   ____   |    / /__\ \    |  |
+         |  |   \  \ |  | |  | |  |    |  |   / ______ \   |  |
+         |  |    \  \|  | |  | |  |    |  |  / /      \ \  |  |_________
+         |__|     \_____| |__| |__|    |__| /_/        \_\ |____________|
+*/
+
+#include <bits/stdc++.h>
+#define ll              long long
+#define ull             unsigned long long
+#define fl              float
+#define db              double
+#define sf(a)           scanf("%d",&a)
+#define sff(a,b)        scanf("%d%d",&a,&b)
+#define sfff(a,b,c)     scanf("%d%d%d",&a,&b,&c)
+#define pf(a)           cout<<a<<endl
+#define fr(i,x,n)       for(int i=x;i<n;i++)
+#define pb(a)           push_back(a)
+#define pii             pair<int,int>
+#define mp(a,b)         make_pair(a,b)
+#define INF             9999999
+
+int n, e;
+using namespace std;
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    sff(n,e);
+    for (int i = 0; i < e; ++i)
+    {
+        int a, b;
+        sff(a,b);
+        if (a>=b)
+        {
+            printf("NO\n");
+            return 0;
+        }
+    }
+    printf("YES\n");
+    return 0;
+}
