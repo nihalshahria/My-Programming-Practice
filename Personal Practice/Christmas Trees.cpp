7145071a715299dd46c794f80b554f9a7c281a40
Md. Shahria Islam Nihal <53012643+nihalshahria@users.noreply.github.com>
2019-12-31 15:01:04 +0600

    nmb

--- Personal Practice/Christmas Trees.cpp
+++ Personal Practice/Christmas Trees.cpp
@@ -11,33 +11,49 @@
 
 #include <bits/stdc++.h>
 #define ll              long long
-#define ull             unsigned long long
-#define fl              float
-#define db              double
-#define sf(a)           scanf("%d",&a)
+#define sf(a)           scanf("%lld",&a)
 #define sff(a,b)        scanf("%d%d",&a,&b)
-#define sfff(a,b,c)     scanf("%d%d%d",&a,&b,&c)
-#define pf(a)           cout<<a<<endl
-#define fr(i,x,n)       for(int i=x;i<n;i++)
 #define pb(a)           push_back(a)
-#define pii             pair<int,int>
+#define pll				pair<ll,ll>
 #define mp(a,b)         make_pair(a,b)
-#define INF             9999999
-
-
 using namespace std;
-
-
-
 int main(){
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    #endif
+	int n, m;
+	sff(n, m);
+	ll a;
+	std::map<ll, ll> pos;
+	std::vector<ll> v;
+	std::queue<pll>q;
+	for (int i = 0; i < n; ++i) {
+		sf(a);
+		q.push(mp(a, 0));
+		pos[a] = 1;
+	}
+	ll sum = 0;
+	while (!q.empty()) {
+		ll x = q.front().first;
+		ll y = q.front().second;
+		q.pop();
+		if (!m) break;
+		if (!pos[x-1]) {
+			v.pb(x-1);
+			pos[x-1] = 1;
+			sum = sum + 1+y;
+			q.push(mp(x-1, y+1));
+			m--;
+		}
+		if (!m) break;
+		if (!pos[x+1]) {
+			v.pb(x+1);
+			pos[1 + x] = 1;
+			sum = sum + 1+y;
+			q.push(mp(x+1, y+1));
+			m--;
+		}
 
-   int a;
-   cin>>a;
-   cout<<a+10;
-
-    return 0;
+	}
+	printf("%lld\n", sum);
+	for (int i = 0; i < v.size(); ++i)
+		printf("%lld ", v[i]);
+	return 0;
 }
--- Personal Practice/Contest_A.cpp
+++ Personal Practice/Contest_A.cpp
@@ -14,30 +14,56 @@
 #define ull             unsigned long long
 #define fl              float
 #define db              double
-#define sf(a)           scanf("%d",&a)
+#define sf(a)           scanf("%lld",&a)
 #define sff(a,b)        scanf("%d%d",&a,&b)
 #define sfff(a,b,c)     scanf("%d%d%d",&a,&b,&c)
-#define pf(a)           cout<<a<<endl
+#define pf(a)           printf("%d\n",a)
 #define fr(i,x,n)       for(int i=x;i<n;i++)
 #define pb(a)           push_back(a)
 #define pii             pair<int,int>
 #define mp(a,b)         make_pair(a,b)
-#define INF             9999999
-
+#define INF             99999999
 
 using namespace std;
 
-
-
 int main(){
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    #endif
-
-   int a;
-   cin>>a;
-   cout<<a+10;
-
-    return 0;
+	int n, m;
+	sff(n,m);
+	long long int a[n];
+	std::map<long long, int> pos;
+	std::vector<long long int> v;
+	for (int i = 0; i < n; ++i){
+		sf(a[i]);
+		pos[a[i]] = 1;
+	}
+	long long int sum = 0, x = 0;
+	while(m>0){
+		x++;
+		for (int i = 0; i < n; ++i)
+		{
+			if (pos[a[i]-x]==0)
+			{
+				v.pb(a[i]-x);
+				pos[a[i]-x]=1;
+				sum = sum + x;
+				m--;	
+			}
+			if (m==0)
+				break;
+			if (pos[a[i]+x]==0)
+			{
+				v.pb(a[i]+x);
+				pos[a[i]+x]=1;
+				sum = sum + x;	
+				m--;
+			}
+		}
+	}
+	printf("%lld\n", sum);
+	for (int i = 0; i < v.size(); ++i)
+	{
+		//cout<<v[i]<<" ";
+		printf("%lld ", v[i]);
+	}
+	return 0;
 }
