a79886495c37432050698987cc1f6ad2b443ada3
Md. Shahria Islam Nihal <53012643+nihalshahria@users.noreply.github.com>
2020-02-07 01:32:51 +0600

    i

--- Personal Practice/test04.cpp
+++ Personal Practice/test04.cpp
@@ -21,43 +21,18 @@
 #define pii             pair<int,int>
 #define mp(a,b)         make_pair(a,b)
 #define Pi              acos(-1)
-#define mx              100001
-using namespace std;
-int arr[mx];
-int seg[mx * 4];
-void Build(int node, int b, int e) {
-    if (b == e) {
-        seg[node] = arr[b];
-        return;
-    }
-    int left = node * 2;
-    int right = node * 2 + 1;
-    int mid = (b + e) / 2;
-    Build(left, b, mid);
-    Build(right, mid + 1, e);
-    seg[node] = seg[left] + seg[right];
-}
-int Query(int node, int b, int e, int l, int r) {
-    if (l > e || r < b)return 0;
-    if (b >= l && e <= r)return seg[node];
-    int left = node * 2;
-    int right = node * 2 + 1;
-    int mid = (b + e) / 2;
-    int p1 = Query(left, b, mid, l, r);
-    int p2 = Query(right, mid + 1, e, l, r);
-    return p1 + p2;
-}
-int main() {
-    int t;
-    sf(t);
-    while (t--) {
-        int n, k;
-        sff(n,k);
-        memset(arr, 0, n+2);
-        memset(seg, 0, (n * 4)+2);
-        for (int i = 0; i < n; ++i)sf(arr[i]);
-        Build(1, 0, n-1);
-        //int x = Query(1, 1, n, l, r);
+#define INF             9999999
 
-    }
+using namespace std;
+int main(){/*
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif*/
+    int n;
+    cin>>n;
+    cout<<n+10<<endl;
+
+
+    return 0;
 }
