6662985e9dbaad8114bb56e37d34a1ac03f11d7a
nihalshahria <nihalshahria@gmail.com>
2021-05-28 19:58:40 +0600

    cmnt add

--- PreCode/KMP_pattern_match.cpp
+++ PreCode/KMP_pattern_match.cpp
@@ -1,24 +1,35 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-void computeLPS(string &pat, int &l2, std::vector<int>& v) {
-	for (int i = 1; i < l2; i++) {
+void computeLPS(string &pat, int &l2, std::vector<int> &v)
+{
+	for (int i = 1; i < l2; i++)
+	{
 		int j = v[i - 1];
-		while (j > 0 && pat[i] != pat[j])j = v[j - 1];
-		if (pat[i] == pat[j])j++;
+		while (j > 0 && pat[i] != pat[j])
+			j = v[j - 1]; //* aabaaab 0101223
+		if (pat[i] == pat[j])
+			j++;
 		v[i] = j;
 	}
 }
-void kmp(string &str, string &pat) {
+void kmp(string &str, string &pat)
+{
 	int l1 = str.size(), l2 = pat.size(), i = 0, j = 0, f = 0;
 	std::vector<int> v(l2, 0);
 	computeLPS(pat, l2, v);
-	
-	while (i < l1) {
-		if (pat[j] == str[i])j++,i++;
-		if (j == l2)printf("%d found\n", i - j + 1), j = v[j-1], f++;
-		else if (i < l1 && pat[j] != str[i]) {
-			if (j)j = v[j - 1];
-			else i++;
+
+	while (i < l1)
+	{
+		if (pat[j] == str[i])
+			j++, i++;
+		if (j == l2)
+			printf("%d found\n", i - j + 1), j = v[j - 1], f++;
+		else if (i < l1 && pat[j] != str[i])
+		{
+			if (j)
+				j = v[j - 1];
+			else
+				i++;
 		}
 	}
 }
