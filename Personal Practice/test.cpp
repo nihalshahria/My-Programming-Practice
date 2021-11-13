b37d394b9b7751809e6a9f62f0de5445e0ab33bb
nihalshahria <nihalshahria@gmail.com>
2021-11-14 00:12:00 +0600

    test

--- Personal Practice/test.cpp
+++ Personal Practice/test.cpp
@@ -1,53 +1,58 @@
-/*
-          _____       __   __   __      __                  __
-         |     \     |  | |  | |  |    |  |       /\       |  |
-         |  |\  \    |  | |  | |  |    |  |      /  \      |  |
-         |  | \  \   |  | |  | |  |____|  |     / /\ \     |  |
-         |  |  \  \  |  | |  | |   ____   |    / /__\ \    |  |
-         |  |   \  \ |  | |  | |  |    |  |   / ______ \   |  |
-         |  |    \  \|  | |  | |  |    |  |  / /      \ \  |  |_________
-         |__|     \_____| |__| |__|    |__| /_/        \_\ |____________|
-*/
+/**
+ *    created: 2021-11-13 23:49:52
+**/
 #include <bits/stdc++.h>
-#define sf(a)               scanf("%d",&a)
-#define sfl(a)              scanf("%lld",&a)
-#define sff(a,b)            scanf("%d %d",&a,&b)
-#define sffl(a,b)           scanf("%lld %lld",&a,&b)
-#define sfff(a,b,c)         scanf("%d %d %d",&a,&b,&c)
-#define sfffl(a,b,c)        scanf("%lld %lld %lld",&a,&b,&c)
-#define sffff(a,b,c,d)      scanf("%d %d %d %d",&a,&b,&c,&d)
-#define sffffl(a,b,c,d)     scanf("%lld %lld %lld %lld",&a,&b,&c,&d)
-#define sfffff(a,b,c,d,e)   scanf("%d %d %d %d %d",&a,&b,&c,&d,&e)
-#define sfffffl(a,b,c,d,e)  scanf("%lld %lld %lld %lld %lld",&a,&b,&c,&d,&e)
-#define MEM(a,b)            memset(a, (b), sizeof(a))
-#define sfc(a)              scanf("%c",&a)
-#define pb(a)               push_back(a)
+using namespace std;
+/*---------------------------------PB_DS---------------------------------*/
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+// #define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+// #define ordered_multiset tree<pair<int, int>, null_type,less<pair<int, int> >, rb_tree_tag,tree_order_statistics_node_update>
+// find_by_order(k) = returns an iterator to the k-th largest element (counting from zero)
+// order_of_key(k) = the number of items in a set that are strictly smaller than k.
+/*-----------------------------------------------------------------------*/
+#define nl                  "\n"
 #define X                   first
 #define Y                   second
+#define str_to_int(x)       stoi(x)
 #define sz(x)               x.size()
-#define pii                 pair<int,int>
-#define pli                 pair<long long int,int>
-#define mp(a,b)             make_pair(a,b)
-#define fr(i,x,n)           for(int i=x;i<n;i++)
-#define rfr(i,x,n)          for(int i=x;i>n;i--)
-#define LCM(a, b)           ((a)*((b)/GCD(a,b)))
-template<typename T>T Abs(T a){return (a<0?-a:a);}
-template<typename T>T MAX(T a, T b){return (a>b?a:b);}
-template<typename T>T MIN(T a, T b){return (a<b?a:b);}
-template<typename T>T GCD(T a, T b){if(b==0)return a;return GCD(b,a%b);}
-template<typename T>inline void read(T &x){
-    T f=1;char c;x=0;
-    for(c=getchar();c<'0'||c>'9';c=getchar())if(c=='-')f=-1;
-    for(;c>='0'&&c<='9';c=getchar())x=x*10+c-'0';
-    x*=f;
-}
-using fl = float;
+#define pb                  push_back
+#define all(v)              v.begin(),v.end()
+#define fast()              ios_base::sync_with_stdio(false);cin.tie(NULL)
 using db = double;
 using ll = long long;
 using ull = unsigned long long;
+/*---------------------------------Debugger---------------------------------*/
+void __print(ll x) {cerr << x;}
+void __print(db x) {cerr << x;}
+void __print(int x) {cerr << x;}
+void __print(ull x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? ", " : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr <<__func__<<":"<<__LINE__<< "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+/*--------------------------------------------------------------------------*/
 const int mx = 1000005;
-const int inf = 99999999;
-const int intlim = 2147483648;
+const int inf = 0x3f3f3f3f;
+const int intlim = 0x7fffffff;
 const db PI = acos(-1); //3.14159265358979323846264338328
 /*------------------------------Graph Moves----------------------------*/
 //const int fx[]={+1,-1,+0,+0};
@@ -57,27 +62,35 @@ const db PI = acos(-1); //3.14159265358979323846264338328
 //const int fx[]={-2, -2, -1, -1,  1,  1,  2,  2};  // Knights Move
 //const int fy[]={-1,  1, -2,  2, -2,  2, -1,  1}; // Knights Move
 /*---------------------------------------------------------------------*/
-using namespace std;
-int main(){
-    int n;
-    sf(n);
-    int a[n];
-    for (int i = 0; i < n; ++i)
-    {
-        sf(a[i]);
+//#ifndef ONLINE_JUDGE
+//freopen("input.txt", "r", stdin);
+//freopen("output.txt", "w", stdout);
+//#endif
+int nCr(int n, int r){
+    if(!r)return 1;
+    return (nCr(n-1, r-1)*n)/r;
+}
+int Pow(int a, int b, int mod = intlim){
+    int ans = 1;
+    while(b){
+        if(b & 1)ans = (1ll * ans * a) % mod;
+        b>>=1;
+        a = (1ll * a * a) % mod;
     }
-    ll x = -1;
-    ll ans = 0;
-    int m;
-    sf(m);
-    while(m--){
-        int h, w;
-        sff(w,h);
-        if(w>x)
-            if(ans<a[w-1])
-                x = w;
-        cout<<ans<<" ";
-        ans+=h;
+    return ans;
+}
+// a function to sieve in linear time
+
+
+void solve(){
+    
+}
+int main(){
+    fast();
+    int t = 1;
+    // cin>>t;
+    for(int i = 1; i<=t; ++i){
+        solve();
     }
     return 0;
 }
