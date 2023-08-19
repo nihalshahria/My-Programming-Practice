ca896a71e737c4978b4f23ef1b8530cad52d852c
nihalshahria <nihalshahria@gmail.com>
2023-08-19 18:16:28 +0600

    Minor change in DSU

--- PreCode/Disjoint_set_union.cpp
+++ PreCode/Disjoint_set_union.cpp
@@ -2,24 +2,34 @@
 using namespace std;
 const int mx = 100005;
 /* <-------- Disjoint Set Union --------> */
-vector<int>parent(mx);
-vector<int>size(mx);
-void make_set(int v){
-    parent[v] = v;
-    size[v] = 1;
-}
-int find_set(int v){
-    if(parent[v]==v)return v;
-    return parent[v] = find_set(parent[v]);
-}
-void union_sets(int a, int b){
-    a = find_set(a);
-    b = find_set(b);
-    if(a==b)return;
-    if(size[a]<size[b])swap(a,b);
-    parent[b] = a;
-    size[a]+=size[b];
-}
+class DSU
+{
+public:
+    vector<int> parent;
+    vector<int> size;
+    int maxSize;
+
+    DSU(int n){
+        size.resize(n, 1);
+        for (int i = 0; i < n; i++)
+            parent.push_back(i);
+    }
+
+    int find_set(int v){
+        if (parent[v] != v)parent[v] = find_set(parent[v]);
+        return parent[v];
+    }
+
+    bool unite_sets(int a, int b){
+        a = find_set(a), b = find_set(b);
+        if (a == b)return false;
+        if (size[a] < size[b])swap(a, b);
+        parent[b] = a;
+        size[a] += size[b];
+        maxSize = max(maxSize, size[a]);
+        return true;
+    }
+};
 /* <------------------------------------> */
 int main(int argc, char const *argv[])
 {
--- PreCode/MST_kruskal.cpp
+++ PreCode/MST_kruskal.cpp
@@ -1,47 +1,52 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 const int mx = 100005;
 /* <-------- Disjoint Set Union --------> */
-vector<int>parent(mx);
-vector<int>size(mx);
-void make_set(int v){
-    parent[v] = v;
-    size[v] = 1;
-}
-int find_set(int v){
-    if(parent[v]==v)return v;
-    return parent[v] = find_set(parent[v]);
-}
-void union_sets(int a, int b){
-    a = find_set(a);
-    b = find_set(b);
-    if(a==b)return;
-    if(size[a]<size[b])swap(a,b);
-    parent[b] = a;
-    size[a]+=size[b];
-}
+class DSU
+{
+public:
+    vector<int> parent;
+    vector<int> size;
+    int maxSize;
+
+    DSU(int n){
+        size.resize(n, 1);
+        for (int i = 0; i < n; i++)
+            parent.push_back(i);
+    }
+
+    int find_set(int v){
+        if (parent[v] != v)parent[v] = find_set(parent[v]);
+        return parent[v];
+    }
+
+    bool unite_sets(int a, int b){
+        a = find_set(a), b = find_set(b);
+        if (a == b)return false;
+        if (size[a] < size[b])swap(a, b);
+        parent[b] = a;
+        size[a] += size[b];
+        maxSize = max(maxSize, size[a]);
+        return true;
+    }
+};
 /* <------------------------------------> */
 int main(int argc, char const *argv[])
 {
-    for(int i = 0; i < mx; ++i)make_set(i);
     int n, m, u, v, w, cost = 0;
-    cin>>n>>m;
-    set<vector<int>>edges;
+    cin >> n >> m;
+    DSU dsu(n);
+    set<vector<int>> edges;
     for (int i = 0; i < m; i++){
-        cin>>u>>v>>w;
-        edges.insert({w,u,v});
+        cin >> u >> v >> w;
+        edges.insert({w, u, v});
     }
-    for(auto i: edges){
+    for (auto i : edges){
         int w = i[0], u = i[1], v = i[2];
-        int x = find_set(u);
-        int y = find_set(v);
-        if(x==y)continue;
-        else{
-            cout<<u<<" "<<v<<"\n";
-            cost+=w;
-            union_sets(u,v);
+        if(dsu.unite_sets(u, v)){
+            cost += w;
         }
     }
-    cout<<cost<<"\n";
+    cout << cost << "\n";
     return 0;
 }
